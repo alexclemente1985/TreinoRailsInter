8cadb76cdb8ce2d53c41d19f520957b7978903c1
Alexandre <alexclemente1985@gmail.com>
2019-01-15 14:46:05 -0200

    implementação de login e logout

--- minitext/config/routes.rb
+++ minitext/config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   
   
+  get 'sessions/new'
   root              'static_pages#home'
   get 'about'   =>  'static_pages#about' #nomeação de rotas (named roots) -> troca / por #
   get 'help'    =>  'static_pages#help'
   get 'contact' =>  'static_pages#contact'
   get 'signup'  =>  'users#new'
+  get     'login'   =>  'sessions#new'
+  post    'login'   =>  'sessions#create'
+  delete  'logout'  => 'sessions#destroy'  
   #rotas restful
   resources          :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
